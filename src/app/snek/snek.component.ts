010a8aaebe2013a7e2186a605d24e870eb0d047e
Tiresa Dombrowski <35544695+tjdombrowski@users.noreply.github.com>
2019-05-08 14:50:32 -0500

    Making a thing

---
+++ src/app/snek/snek.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-snek',
+  templateUrl: './snek.component.html',
+  styleUrls: ['./snek.component.css']
+})
+export class SnekComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
