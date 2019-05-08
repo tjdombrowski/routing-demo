51796146f4a022d9815f6de8a4e2fb80d2ba8811
Tiresa Dombrowski <35544695+tjdombrowski@users.noreply.github.com>
2019-05-08 14:54:09 -0500

    Adding routes to routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SnekComponent } from './snek/snek.component';
+import { TreeComponent } from './tree/tree.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: SnekComponent},
+  {path: "snek", component: SnekComponent},
+  {path: "tree", component: TreeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
