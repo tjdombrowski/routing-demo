4b3d0d34735311a8242b8eefbefca504f65d6abf
tjdombrowski <tjdombrowski@gmail.com>
2019-05-12 14:24:49 -0500

    Added routing elements to cat component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SnekComponent } from './snek/snek.component';
 import { TreeComponent } from './tree/tree.component';
+import { CatComponent } from './cat/cat.component';
 
 const routes: Routes = [
   {path: "", component: SnekComponent},
   {path: "snek", component: SnekComponent},
-  {path: "tree", component: TreeComponent}
+  {path: "tree", component: TreeComponent},
+  {path: "cat/:id", component: CatComponent},
+  {path: "cat", component: CatComponent},
 ];
 
 @NgModule({
--- src/app/cat/cat.component.ts
+++ src/app/cat/cat.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-cat',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CatComponent implements OnInit {
 
-  constructor() { }
+  getCatName() {
+    const name = this.route.snapshot.paramMap.get('id');
+    return name;
+  }
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
   }
